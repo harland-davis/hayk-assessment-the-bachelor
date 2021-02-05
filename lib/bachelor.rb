7e145a704c8c996f228cb95ae0add6578accae38
Kyle Davis <kyledavis@Kyles-iMac.local>
2021-02-05 14:53:09 -0700

    commit tree 415d2f765cc129c4f8e3472c80a1f50ede06bd05

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,72 @@
-def get_first_name_of_season_winner(data, season)
-  # code here
+require 'pry'
+require 'json'
+file = File.read('./spec/fixtures/contestants.json')
+contestants = JSON.parse(file)
+
+
+def get_all_contestants(data)
+  data.values.flatten 
 end
+
+
+def get_first_name_of_season_winner(data, season)
+  matching_contestant = data[season].find do |contestant|
+    contestant["status"] == "Winner"
+  end
+  matching_contestant["name"].split[0]
+end
+
+  # matching_contestant = get_all_contestants(data).find do |contestant|
+  #   contestant["occupation"] == occupation
+  # end
+  # matching_contesant["name"]
 
 def get_contestant_name(data, occupation)
-  # code here
+  matching_contestant = get_all_contestants(data).find do |contestant|
+    contestant["occupation"] == occupation
+    end
+    matching_contestant["name"]
 end
 
+# get_all_contestants(data).reduce(0) do |count, contestant|
+#   if contestant["hometown"] == hometown
+#     count + 1
+#   else
+#     count
+#   end
+# end
+# end
+
+# get_all_contestants(data).filter do |contestant| 
+#   contestant["hometown"] == hometown
+# end 
+# contestants = get_all_contestants(data)
+# contestants.filter { |contestant|contestant["hometown"] == hometown}.length
+
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  contestants_by_hometown = get_all_contestants(data).filter do |contestant|
+    contestant["hometown"] == hometown
+  end
+  contestants_by_hometown.length 
 end
 
 def get_occupation(data, hometown)
-  # code here
+  contestant_by_occupation = get_all_contestants(data).find do |contestant|
+    contestant["hometown"] == hometown
+  end
+  contestant_by_occupation["occupation"]
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum_of_all_contestants_ages = data[season].sum do |contestant|
+    contestant["age"].to_f
+  end
+  all_ages_of_contestants = data[season].length
+  (sum_of_all_contestants_ages/all_ages_of_contestants).round
 end
+
+
+
+# binding.pry
+# 0 
+
